2ac0b382ca3fa00b4d79bc9978970a0f07c44356
Erick-XMB <erick.martinezkmn@ciencias.unam.mx>
2026-04-21 10:59:21 -0600

    primero

---
+++ ArbolBinarioCompleto.java
@@ -0,0 +1,43 @@
+public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
+    
+    private Vertice ultimoAgregado;
+
+    public ArbolBinarioCompleto() {
+        super();
+        ultimoAgregado = null;
+    }
+
+    @Override
+    public void agregar(T elemento) {
+        // Aquí va tu codigo
+    }
+
+    @Override
+    public void eliminar(T elemento) {
+        // Aquí va tu codigo
+    }
+
+    private void actualizarUltimoAgregado() {
+        // Aquí va tu codigo
+    }
+
+
+    @Override
+    public boolean buscar(T elemento) {
+        // Aquí va tu codigo
+    }
+
+    private boolean buscar(T elemento, Vertice v){
+        // Aquí va tu codigo
+    }
+
+    @Override
+    public ListaDoblementeLigada<T> devolverRecorrido() {
+        // Aquí va tu codigo
+    }
+
+    private void agregaEnPrimerEspacioIzquierdo(Vertice desde, Vertice nuevo) {
+        // Aquí va tu codigo
+    }
+
+}
