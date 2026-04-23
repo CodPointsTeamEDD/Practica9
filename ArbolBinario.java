b3d1a781e3be858bdf401d9840b37a6f07910a47
Erick-XMB <erick.martinezkmn@ciencias.unam.mx>
2026-04-22 21:52:36 -0600

    estructura arbol

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -1,17 +1,44 @@
 import java.util.Iterator;
+import listas.ListaDoblementeLigada;
+import tdas.Coleccion;
 
+/**
+ * Clase asbtracta {@code ArbolBinario}
+ * Implementa la interfaz {@link Coleccion}
+ * 
+ * @author Luis Fernando Quintana López
+ * @author Erick Xavier Martinez Briones
+ * @version 1.0.0
+ * @since 2026
+ * 
+ */
 public abstract class ArbolBinario<T> implements Coleccion<T> {
 
+
+    /**
+     * Clase protegida {@code Vertice}
+     * Vertice para poder usar en la clase ArbolBinario
+     */
     protected class Vertice {
 
+        /**Atributos de la clase Vertice */
+
+        /** Elemento de tipo T del vertice */
         public T elemento;
 
+        /** Vertice que representa el vertice izquierdo del vertice dado */
         public Vertice izquierdo;
 
+        /** Vertice que representa el vertice derecho del vertice dado */
         public Vertice derecho;
 
+        /** Vertice que representa el padre del vertice dado */
         public Vertice padre;
 
+        /**
+         * Constrcutor de la clase Vertice
+         * @param elemento elemento dentro del vertice a crear
+         */
         public Vertice(T elemento) {
             this.elemento = elemento;
             this.izquierdo = null;
@@ -19,59 +46,113 @@ public abstract class ArbolBinario<T> implements Coleccion<T> {
             this.padre = null;
         }
 
+        /**
+         * Metodo para determinar si el vertice dado es hijo izquierdo
+         * {true} si es hijo izquierdo, {false} en otro caso
+         */
         public boolean esHijoIzquierdo() {
             return padre != null && padre.izquierdo == this;
         }
 
+        /**
+         * Metodo para determinar si el vertice dado es hijo derecho
+         * {true} si es hijo derechi, {false} en otro caso
+         */
         public boolean esHijoDerecho() {
             return padre != null && padre.derecho == this;
         }
 
+        /**
+         * Agrega un vértice como hijo izquierdo de este vértice
+         * @param hijo el vertice que se asignara como hijo izquierdo
+         */
         public void agregaIzquierdo(Vertice hijo) {
             izquierdo = hijo;
-            if (hijo != null){ 
+            if (hijo != null) {
                 hijo.padre = this;
             }
         }
 
+        /**
+         * Agrega un vértice como hijo derecho de este vértice
+         * @param hijo el vertice que se asignara como hijo derecho
+         */
         public void agregaDerecho(Vertice hijo) {
             derecho = hijo;
-            if (hijo != null) hijo.padre = this;
+            if (hijo != null)
+                hijo.padre = this;
         }
     }
 
+    /** atributos de la clase ArbolBinario */
+
+    /** Vertice raiz que representa la raiz del arbol binario */
     protected Vertice raiz;
 
+    /** Numero natural que representa el tamanio del arbol binario */
     protected int tamanio;
 
+    /**
+     * Constructor de la clase ArbolBinario
+     * Inicializa la raiz como null
+     * Inicializa el tamanio como0
+     */
     public ArbolBinario() {
         this.raiz = null;
         this.tamanio = 0;
     }
 
+    /**
+     * Metodo abstracto que busca un elemento de tipo T en el arbol binario
+     */
     @Override
     public abstract boolean buscar(T elemento);
 
+    /**
+     * Metodo acceder que accede a un elemento del arbol binario dado un indice
+     * @param indice indice al cual queremos acceder
+     * @return el elemento en el indice seleccionado 
+     */
     public T acceder(int indice) {
-        // Aquí va tu codigo
+        ListaDoblementeLigada<T> recorrido = this.devolverRecorrido();
+        return recorrido.acceder(indice);
     }
 
+    /**
+     * Metodo que devuelve el tamanio del arbol
+     * @return el atributo del tamanio del arbol
+     */
     public int devolverTamanio() {
-        // Aquí va tu codigo
+        return this.tamanio;
     }
 
+    /** 
+     * Metodo que determina si el arbol esta vacio 
+     * {true} si el arbol esta vacio (con tamanio 0), {false} en otro caso
+     */
     public boolean estaVacio() {
-        // Aquí va tu codigo
+        if (this.devolverTamanio() == 0) {
+            return true;
+        }
+
+        return false;
     }
 
+    /**Metodo abstracto que agrega un elemento de tipo T al arbol binario */
     @Override
     public abstract void agregar(T elemento);
 
+    /** Metodo abstracto que elimina un elemento de tipo T del arbol binario */
     @Override
     public abstract void eliminar(T elemento);
 
+    /** Metodo abstrato que devuelve el recorrido del arbol binario siguiendo la metodologia BFS */
     protected abstract ListaDoblementeLigada<T> devolverRecorrido();
-
+    
+    /** Metodo que devuelve un inteardor para recorrer los elementos del arbol
+     * Se realiza en el orden definido por devolverRecorrido()
+     * @return un iterador sobre los elementos del arbol
+     */
     @Override
     public Iterator<T> iterator() {
         return devolverRecorrido().iterator();
--- ArbolBinarioCompleto.java
+++ ArbolBinarioCompleto.java
@@ -1,5 +1,8 @@
+import colas.Cola;
+import listas.ListaDoblementeLigada;
+
 public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
-    
+
     private Vertice ultimoAgregado;
 
     public ArbolBinarioCompleto() {
@@ -9,35 +12,168 @@ public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
 
     @Override
     public void agregar(T elemento) {
-        // Aquí va tu codigo
+        if (elemento == null) {
+            throw new IllegalArgumentException("El elemento no puede ser vacio");
+        }
+
+        Vertice nuevo = new Vertice(elemento);
+
+        if (this.estaVacio()) {
+            this.raiz = nuevo;
+            this.ultimoAgregado = nuevo;
+            this.tamanio++;
+
+        } else {
+
+            Vertice vi = this.ultimoAgregado;
+
+            if (vi.esHijoIzquierdo()) {
+                vi.padre.agregaDerecho(nuevo);
+                this.ultimoAgregado = nuevo;
+                this.tamanio++;
+                return;
+            }
+
+            while (vi.esHijoDerecho()) {
+                vi = vi.padre;
+            }
+
+            if (vi.esHijoDerecho() || vi.esHijoIzquierdo()) {
+                vi = vi.padre.derecho;
+            }
+
+            this.agregaEnPrimerEspacioIzquierdo(vi, nuevo);
+            return;
+        }
     }
 
     @Override
     public void eliminar(T elemento) {
-        // Aquí va tu codigo
+        if (elemento == null || this.estaVacio()) {
+            throw new IllegalArgumentException("Error al querer eliminar dicho elemento");
+        }
+
+        Vertice verticeAEliminar = null;
+        Vertice ultimoVertice = null;
+        Cola<Vertice> cola = new Cola<>();
+        cola.meter(this.raiz);
+
+        while (!cola.estaVacia()) {
+            Vertice actual = cola.sacar();
+            ultimoVertice = actual;
+
+            if (actual.elemento.equals(elemento)) {
+                verticeAEliminar = actual;
+            }
+
+            if (actual.izquierdo != null) {
+                cola.meter(actual.izquierdo);
+            }
+
+            if (actual.derecho != null) {
+                cola.meter(actual.derecho);
+            }
+        }
+
+        if (verticeAEliminar == null) {
+            return;
+        }
+
+        verticeAEliminar.elemento = ultimoVertice.elemento;
+
+        if (ultimoVertice.padre == null) {
+            this.raiz = null;
+        } else {
+            Vertice p = ultimoVertice.padre;
+            if (p.izquierdo.equals(ultimoVertice)) {
+                p.izquierdo = null;
+            } else {
+                p.derecho = null;
+            }
+        }
+
+        this.tamanio--;
+        this.actualizarUltimoAgregado();
     }
 
     private void actualizarUltimoAgregado() {
-        // Aquí va tu codigo
-    }
+        if (this.estaVacio()) {
+            this.ultimoAgregado = null;
+            return;
+        }
 
+        Cola<Vertice> cola = new Cola<>();
+        cola.meter(this.raiz);
+        Vertice ultimo = null;
+
+        while (!cola.estaVacia()) {
+            ultimo = cola.sacar();
+
+            if (ultimo.izquierdo != null) {
+                cola.meter(ultimo.izquierdo);
+            }
+
+            if (ultimo.derecho != null) {
+                cola.meter(ultimo.derecho);
+            }
+        }
+
+        this.ultimoAgregado = ultimo;
+    }
 
     @Override
     public boolean buscar(T elemento) {
-        // Aquí va tu codigo
+        return buscar(elemento, this.raiz);
     }
 
-    private boolean buscar(T elemento, Vertice v){
-        // Aquí va tu codigo
+    private boolean buscar(T elemento, Vertice v) {
+        if (v == null) {
+            return false;
+        }
+
+        if (v.elemento.equals(elemento)) {
+            return true;
+        }
+
+        return buscar(elemento, v.izquierdo) || buscar(elemento, v.derecho);
     }
 
     @Override
     public ListaDoblementeLigada<T> devolverRecorrido() {
-        // Aquí va tu codigo
+        ListaDoblementeLigada<T> recorrido = new ListaDoblementeLigada<>();
+
+        if (estaVacio()) {
+            return recorrido;
+        }
+
+        Cola cola = new Cola<>();
+        cola.meter(this.raiz);
+
+        while (!cola.estaVacia()) {
+            Vertice actual = (Vertice) cola.sacar();
+            recorrido.agregarFinal(actual.elemento);
+
+            if (actual.izquierdo != null) {
+                cola.meter(actual.izquierdo);
+            }
+
+            if (actual.derecho != null) {
+                cola.meter(actual.derecho);
+            }
+        }
+        return recorrido;
     }
 
     private void agregaEnPrimerEspacioIzquierdo(Vertice desde, Vertice nuevo) {
-        // Aquí va tu codigo
+
+        while (desde.izquierdo != null) {
+            desde = desde.izquierdo;
+        }
+
+        desde.agregaIzquierdo(nuevo);
+        this.ultimoAgregado = nuevo;
+        this.tamanio++;
+        return;
     }
 
 }
